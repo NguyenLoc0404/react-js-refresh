e573f5ce5e68e500970044b7ae0cc00eabc8412e
NguyenLoc0404 <nttloc0404git@gmail.com>
2024-01-18 17:06:53 +0700

    fix error in bootrsap with gitingnore file

--- my-react-app/src/Product.js
+++ my-react-app/src/Product.js
@@ -1,3 +1,36 @@
+import { useState } from 'react';
 const Products = () => {
-    
+    const [products, setProducts] = useState([
+        { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
+        { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
+        { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
+        { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
+        { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
+        { category: "Vegetables", price: "$1", stocked: true, name: "Peas" },
+      ]);
+
+      return (
+        <div>
+          <h2>category</h2>
+          <table>
+            <tbody>
+              <tr>
+                <th>Company</th>
+                <th>Contact</th>
+                <th>Country</th>
+              </tr>
+              <tr>
+                <td>Alfreds Futterkiste</td>
+                <td>Maria Anders</td>
+                <td>Germany</td>
+              </tr>
+              <tr>
+                <td>Centro comercial Moctezuma</td>
+                <td>Francisco Chang</td>
+                <td>Mexico</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      );
 }
--- my-react-app/src/Search.js
+++ my-react-app/src/Search.js
@@ -1,3 +1,11 @@
 const Search = () => {
-    
-}
+  return (
+    <div style={{ display: "block" }}>
+      <input classname="form-control" type="text" placeholder="Search..." />
+      <br />
+      <input classname="form-check-input" type="checkbox" id="dropdownCheck" />
+      <label classname="form-check-label">Only show products in stock</label>
+    </div>
+  );
+};
+export default Search;
